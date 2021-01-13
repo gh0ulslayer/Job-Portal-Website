eb715e665b96351a99d0111ff29544dec957a766
Aman <amanmeel1911@gmail.com>
2021-01-13 18:36:15 +0530

    on process..

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React , { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AppNavbar from './components/Appnavbar';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
